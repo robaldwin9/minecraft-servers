c4b5516b8170e756058cf16227aadd7f748ee1c6
Ronald Baldwin <robaldwin9@gmail.com>
2026-02-16 20:49:50 -0500

    update whitelist

--- paper/whitelist-bedrock.sh
+++ paper/whitelist-bedrock.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 docker exec -i paper-mc-1  rcon-cli "fwhitelist add 00000000-0000-0000-0009-000006a978c2"
 docker exec -i paper-mc-1  rcon-cli "fwhitelist add 00000000-0000-0000-0009-00000919bf15"
+docker exec -i paper-mc-1  rcon-cli "fwhitelist add 00000000-0000-0000-0009-01f8ba14b200"
+docker exec -i paper-mc-1  rcon-cli "fwhitelist add 00000000-0000-0000-0009-01fb6a371b18"
+
+
 
